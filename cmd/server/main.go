7a8257d3d84b6f5c5ff1ffe1bff02f1a1973beca
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-09-12 16:46:52 +0700

    CH2: Messages Brokers - L3: Connect
    Use my own answer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionStr)
+	if err != nil {
+		fmt.Println("Connection failed")
+		return
+	}
+
+	defer conn.Close()
+	fmt.Println("Connection succeeded")
 	fmt.Println("Starting Peril server...")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Connection closed")
 }
