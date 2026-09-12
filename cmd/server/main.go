fb3942a59b43c237e9ae52f17498cf0cd2eaab08
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-09-12 16:48:48 +0700

    CH2: Messages Brokers - L3: Connect
    Use Boot.Dev's answer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
@@ -9,18 +10,18 @@ import (
 )
 
 func main() {
-	connectionStr := "amqp://guest:guest@localhost:5672/"
-	conn, err := amqp.Dial(connectionStr)
-	if err != nil {
-		fmt.Println("Connection failed")
-		return
-	}
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
 	defer conn.Close()
-	fmt.Println("Connection succeeded")
-	fmt.Println("Starting Peril server...")
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
-	fmt.Println("Connection closed")
+	fmt.Println("RabbitMQ connection closed.")
 }
